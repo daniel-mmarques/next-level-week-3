2328a921ff9e82291105fda26b069734390e2236
Daniel Marques <danielmarianomarquespr@gmail.com>
2020-10-14 14:17:07 -0300

    Segundo dia do Next-level-week-3

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,19 @@
+const map = L.map("mapid").setView([-26.9083339, -49.0798445], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minwidth: 240,
+    minheight: 240
+  }).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>')
+
+L.marker([-26.9083339, -49.0798445], {icon}).addTo(map).bindPopup(popup);
